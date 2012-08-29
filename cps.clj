30b487217dbbd08d4310ca164ae149c214a71ff5
Brandon Bloom <brandon@brandonbloom.name>
2012-08-28 20:47:43 -0700

    Some effort on the anf

--- cps.clj
+++ cps.clj
@@ -1,38 +1,43 @@
-(ns anf
+(ns cps
   (:require [cljs.compiler :as comp]
             [cljs.analyzer :as ana]))
 
-(defn analyze [form]
-  (let [env (ana/empty-env)] ;TODO: How to get the "current" environment?
-    (ana/analyze env form)))
+;; TODO: Put this into the env
+;(def ^:dynamic *k* 'cps/top-level-k)
 
-;;TODO: Namespace these ops
-(def control-ops #{'shift 'reset}) ; Alternatively, call-cc & friends.
+(defmacro call-cc [f & args]
+  (throw (Exception. "call-cc must be used within a cps transform")))
+
+(defmacro return [k value]
+  (throw (Exception. "return must be used within a cps transform")))
+
+(defmacro raise [k error]
+  (throw (Exception. "raise must be used within a cps transform")))
+
+(def control-ops `#{call-cc return raise})
+
+(defn control-op?
+  [{:keys [op info] :as ast}]
+  (and (= op :var) (contains? control-ops (:name info))))
 
 ;;TODO: Possible optimization: annontation pass, this becomes O(1)
 ;; Downside: Must manually re-run annotation pass when re-creating forms.
 ;; Could potentially bypass downside with pluggable hooks in base analyze.
-(defn trivial?
-  "Trivial expressions definitely do not evaluate any control forms."
-  [{:keys [op children] :as ast}]
-  (cond
-    (= op :var) (not (-> ast :info :cps))
-    :else (every? trivial? children)))
-
 (defn serious?
   "Serious expressions might evaluate one or more control forms."
+  [{:keys [op f children] :as ast}]
+  (or (and (= op :invoke) (control-op? f))
+      (boolean (some serious? children))))
+
+(defn trivial?
+  "Trivial expressions definitely do not evaluate any control forms."
   [ast]
-  (not (trivial? ast)))
+  (not (serious? ast)))
 
 (defmulti anf
   "Applies an Administrative Normal Form transformation to an AST.
   The transformation is selective with regard to trivial expressions."
   :op)
-
-(defn anf*
-  "Applies a selective ANF transform to a form."
-  [form]
-  (-> (analyze form) anf :form))
 
 (defmethod anf :default
   [{:keys [env op] :as ast}]
@@ -40,14 +45,22 @@
     (ana/warning env (str "Unsupported op " op " in ANF transform")))
   ast)
 
+(defn- anf-application [env prefix args]
+  (let [syms (repeatedly (count args) #(gensym "anf"))
+        forms (map (comp :form anf) args)]
+    (ana/analyze env `(let [~@(interleave syms forms)]
+                        (~@(map :form prefix) ~@syms)))))
+
+
 (defmethod anf :invoke
-  [{:keys [env children] :as ast}]
-  (if (trivial? ast)
-    ast
-    (let [syms (repeatedly (count children) #(gensym "anf"))
-          forms (map :form children)]
-      (ana/analyze env `(let [~@(interleave syms forms)]
-                          (~@syms))))))
+  [{:keys [env children f args] :as ast}]
+  (if (control-op? f)
+    (if (every? trivial? args)
+      ast
+      (anf-application env [f] args))
+    (if (trivial? ast)
+      ast
+      (anf-application env [] children))))
 
 ;;TODO ALL THE OPS!
 ;(defmethod cps :meta
@@ -71,26 +84,30 @@
 ;(defmethod cps :dot
 ;(defmethod cps :js
 
+(defn- wrap-return
+  [{:keys [env context] :as ast} k]
+  (if (and k (= context :return))
+    (ana/analyze env `(k-return ~k)
+    k)))
+
 (defmulti cps
   "Applies a Continuation Passing Style transformation to an AST.
   The transformation is selective with regard to trivial expressions and
   assumes that serious call-sites are in Administrative Normal Form."
-  :op)
-
-(defn cps*
-  "Applies a selective CPS transform to a form."
-  [form]
-  (-> (analyze form) anf cps :form))
+  (fn [ast k]
+    (:op ast)))
 
 (defmethod cps :default
-  [{:keys [env op] :as ast}]
+  [{:keys [env op] :as ast} k]
   ;;TODO: Error? Generic walk? Something else? See also anf :default
   (ana/warning env (str "Unsupported op " op " in CPS transform"))
   ast)
 
 (defmethod cps :invoke
-  [{:keys [env f args] :as ast}]
-  ast)
+  [{:keys [env f args] :as ast} k]
+  (if (trivial? ast)
+    ast
+    nil))
 
 ;;TODO ALL THE OPS!
 ;(defmethod cps :invoke
@@ -116,21 +133,39 @@
 ;(defmethod cps :dot
 ;(defmethod cps :js
 
+;(defmacro anf*
+;  "Applies a selective ANF transform to a form."
+;  [form]
+;  (-> (ana/analyze &env form) anf :form))
+;
+;(defmacro cps*
+;  "Applies a selective CPS transform to a form."
+;  [form]
+;  (-> (ana/analyze &env form) anf (cps nil) :form))
+
 (comment
+
+(defn analyze [form]
+  (ana/analyze (ana/empty-env) form))
 
 (use '[clojure.pprint :only (pprint)])
 
-(pprint (anf* 'x))
+(trivial? (analyze '(defn f [x] x)))
 
-;; Toggle back and forth between these to test re-defining-away the metadata.
-;; Metadata was bugged on redef in the CLJS compiler and I have a fix.
-(trivial? (analyze '(do (defn f [x] x) (f 1))))
-(trivial? (analyze '(do (defn ^:cps f [x] x) (f 1))))
+(trivial? (analyze '(defn f [] (cps/call-cc identity))))
 
-(pprint (anf* '(f x)))
+(defn show-anf [form]
+  (-> form
+    analyze
+    anf
+    :form
+    pprint))
 
-(analyze '(defn ^:cps cps-inc [x] (inc x)))
 
-(pprint (anf* '(cps-inc x)))
+(show-anf '(identity 1))
+
+(show-anf '(identity 1 (cps/call-cc 2) 3 (cps/call-cc 4)))
+
+;TODO? (trivial? (analyze '(do (defn ^:cps f [x] x) (f 1))))
 
 )
---
+++ cps.cljs
@@ -0,0 +1,31 @@
+(ns cps
+  (:require-macros [cps :only (call-cc)]))
+
+(defprotocol IContinuation
+  ;;TODO: Should these start with a dash and be considered "internal"?
+  (return [_ result])
+  (raise [_ error]))
+
+(deftype Continuation [returnf raisef]
+  IContinuation
+  (return [_ result]
+    (returnf result))
+  (raise [_ error]
+    (raisef error)))
+
+(defn continuation? [x]
+  (instance? Continuation x))
+
+(def top-level-k
+  (Continuation. identity #(throw %)))
+
+
+(comment
+
+(cps* (f (call-cc g)))
+
+(anf* 'x)
+
+(anf* '(f x))
+
+)
